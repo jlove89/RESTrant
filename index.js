c99cc8776ce31dff1b70cf0a2fc15255410a42bb
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-02-17 10:19:16 -0500

    added edit and delete button

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
+
 const app = express()
 
 // Middleware
@@ -8,6 +10,7 @@ app.set('views', __dirname + '/views')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.use('/places', require('./controllers/places'))
 
