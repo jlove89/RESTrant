15a3a00569bb43d0b4d8d6c43b0f58cc43d46ff6
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-02-16 20:12:52 -0500

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -10,7 +10,17 @@ function Def (html) {
             </head>
             <body>
                 <nav>
-                    <ul></ul>
+                    <ul>
+                        <li>
+                        <a href="/">Home</a>
+                        </li>
+                        <li>
+                        <a href="/places">Places</a>
+                        </li>
+                        <li>
+                        <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
                 </nav>
                 {html.children}
             </body>
