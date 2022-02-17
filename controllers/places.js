45a4604fa3386dcd7f4dc9e9d6179f220c76354f
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-02-16 20:39:41 -0500

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -4,9 +4,9 @@ const router = require('express').Router()
 
 const places = require('../models/places')
 
-router.get('/new', (req, res) => {
-    res.render('places/new')
-  })
+//router.get('/new', (req, res) => {
+    //res.render('places/new')
+  //})
 
 router.get('/', (req, res) => {
     let places = [{
@@ -38,6 +38,23 @@ router.post('/', (req, res) => {
     }
     places.push(req.body)
     res.redirect('/places')
+  })
+
+  router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+
+  router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', { place: places[id] })
+    }
   })
 
 module.exports = router
