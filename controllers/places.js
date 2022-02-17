e387cd7650b3d429d273c119b6db7b5dd518e379
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-02-17 11:32:56 -0500

    edit

--- controllers/places.js
+++ controllers/places.js
@@ -4,9 +4,9 @@ const router = require('express').Router()
 
 const places = require('../models/places')
 
-//router.get('/new', (req, res) => {
-    //res.render('places/new')
-  //})
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
 
 router.get('/', (req, res) => {
     let places = [{
