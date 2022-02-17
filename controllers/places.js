10fbec37731b201287206988a287f70ef721bf48
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-02-17 11:21:13 -0500

    added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -57,6 +57,19 @@ router.post('/', (req, res) => {
     }
   })
 
+  router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+      res.render('places/edit', { place: places[id] })
+    }
+  })
+  
   router.delete('/places/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
