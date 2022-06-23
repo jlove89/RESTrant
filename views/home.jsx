5ed1665bf5cab6d3835b4d5a3d021970943762e7
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-06-22 20:14:41 -0400

    Finalized

--- views/home.jsx
+++ views/home.jsx
@@ -5,17 +5,17 @@ function home () {
     return (
         <Def>
             <main>
-                <h1>REST-Rant</h1>
-                <div>
-                    <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
-                    <p>
-                        Photo by <a href="https://unsplash.com/@cravethebenefits?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brenda Godinez</a> on <a href="https://unsplash.com/s/photos/free-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-                    </p>
-                </div>
-                <a href="/places">
-                    <button className="btn-primary">Places Page</button>
-                </a>
-            </main>
+    <h1>HOME</h1>
+    <div >
+      <img height="300" width="500" src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+      <div>
+        Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+      </div>
+    </div>
+    <a href="/places">
+        <button className="btn-primary">Places Page</button>
+    </a>
+</main>
         </Def>
     )
 }
